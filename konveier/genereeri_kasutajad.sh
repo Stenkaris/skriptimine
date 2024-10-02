ca0c7ac95fa5b4921e2da5bef6ce9a30d5c5be61
Sten karis <sten.karis@voco.ee>
2024-10-02 11:05:30 -0400

    muudatused

--- konveier/genereeri_kasutajad.sh
+++ konveier/genereeri_kasutajad.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
+# Sten Karis
+# Skript kasutajate tunnuste ja e-mailide loomiseks failist "inimesed"
+# Kontrolli, kas fail "inimesed" on olemas ja saada failinimi parameetrina
+if [ "$#" -ne 1 ]; then
+    echo "Kasutus: $0 <failinimi>"
+    exit 1
+fi
 
-# Muutuja kataloogiga
-DIR=~/skriptimine/konveier
+inimesed_fail="$1"
 
-# Eesnimed
-cat $DIR/inimesed | cut -d"," -f2 | cut -d";" -f1 | tr '[:upper:]' '[:lower:]' > $DIR/eesnimed.txt
+if [ ! -f "$inimesed_fail" ]; then
+    echo "Viga: Faili '$inimesed_fail' ei leitud!"
+    exit 1
+fi
 
-# Perenimed
-cat $DIR/inimesed | cut -d"," -f1 | tr '[:upper:]' '[:lower:]' > $DIR/perenimed.txt
+# Loo vajalik kataloog
+mkdir -p konveier
+# 6. Eesnimed (peale koma), salvestamine faili eesnimed
+cut -d',' -f2 "$inimesed_fail" | tr -d ' ' > konveier/eesnimed.txt
+# 7. Perenimed (peale :), salvestamine faili perenimed
+cut -d',' -f1 "$inimesed_fail" | tr -d ' ' > konveier/perenimed.txt
+# 8. Domeenid (peale "-"), salvestamine faili domeenid
+cut -d'-' -f2 "$inimesed_fail" > konveier/domeenid.txt
+# 9. Muuda eesnimed väikesteks
+tr '[:upper:]' '[:lower:]' < konveier/eesnimed.txt > konveier/eesnimed_lc.txt
+# 10. Muuda perenimed väikesteks
+tr '[:upper:]' '[:lower:]' < konveier/perenimed.txt > konveier/perenimed_lc.txt
+# 11. Loo kasutajate fail kujul eesnimi.perenimi
+paste -d'.' konveier/eesnimed_lc.txt konveier/perenimed_lc.txt > konveier/kasutajad.txt
+# 12. Loo e-mailide fail kujul eesnimi.perenimi@domeen
+paste -d'.' konveier/eesnimed_lc.txt konveier/perenimed_lc.txt | paste -d'@' - konveier/domeenid.txt > konveier/meilid.txt
 
-# Domeenid
-cat $DIR/inimesed | cut -d":" -f2 > $DIR/domeenid.txt
+echo "Töö tehtud! Failid 'kasutajad.txt' ja 'meilid.txt' on loodud kaustas 'konveier'."
 
-# Kasutajate tunnused
-paste -d"." $DIR/eesnimed.txt $DIR/perenimed.txt > $DIR/kasutajad.txt
-
-# Meilide koostamine
-paste -d"@" $DIR/kasutajad.txt $DIR/domeenid.txt > $DIR/meilid.txt
